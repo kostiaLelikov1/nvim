d94c70b3d9364486e4d121cd192d6f2e58191d16
Kostiantyn <kostiantyn.lelikov@eze.tech>
2024-06-01 09:50:27 +0300

    refactor

---
+++ lua/klelikov/plugins/git.lua
@@ -0,0 +1,23 @@
+return {
+	{
+		'tpope/vim-fugitive',
+	},
+	{
+		'lewis6991/gitsigns.nvim',
+		config = function()
+			local wk = require('which-key')
+			require('gitsigns').setup()
+
+			wk.register({
+				['G'] = {
+					name = '+git',
+					s = { '<cmd>lua require"gitsigns".stage_hunk()<CR>', 'Stage hunk' },
+					u = { '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>', 'Undo stage hunk' },
+					r = { '<cmd>lua require"gitsigns".reset_hunk()<CR>', 'Reset hunk' },
+					R = { '<cmd>lua require"gitsigns".reset_buffer()<CR>', 'Reset buffer' },
+					p = { '<cmd>lua require"gitsigns".preview_hunk()<CR>', 'Preview hunk' },
+				},
+			}, { prefix = '<leader>' })
+		end,
+	},
+}
